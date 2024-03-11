74da8a805ecf3b3f11bc5aba333d34314f78bbd2
Jacek Kotlarski <szumak@virthost.pl>
2024-03-11 18:55:28 +0100

    refactoring

--- checker_libs/checker.py
+++ checker_libs/checker.py
@@ -37,9 +37,11 @@ class Checker(BLogProcessor, BLogClient, BCheckerData):
         self.logger = LogClient(self.qlog)
 
         # logging thread
-        self.thlog = Thread(target=self.th_logger, name=f"{self.pluginname} log worker")
-        self.thlog.daemon = True
-        self.thlog.start()
+        self.th_log = Thread(
+            target=self.th_logger, name=f"{self.pluginname} log worker"
+        )
+        self.th_log.daemon = True
+        self.th_log.start()
 
         self.logger.debug = f"{self.pluginname} object creation complete."
 
