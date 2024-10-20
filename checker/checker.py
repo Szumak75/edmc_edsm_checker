0faa801e3f492e9b50dcd71c145ca75e6f9552eb
Jacek Kotlarski <szumak@virthost.pl>
2024-10-20 13:58:52 +0200

    version update

--- checker/checker.py
+++ checker/checker.py
@@ -27,7 +27,7 @@ class Checker(BLogProcessor, BLogClient, BCheckerData):
         # data
 
         self.plugin_name = "EDSM Checker"
-        self.version = "1.1.0-dev"
+        self.version = "1.1.0"
 
         # logging subsystem
         self.qlog = SimpleQueue()
